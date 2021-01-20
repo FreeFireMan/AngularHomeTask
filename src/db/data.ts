9f3cb9c7dcde71f745ffc4c9698aea478e8f691a
HorutskaL <liliahorutska@gmail.com>
2021-01-20 17:11:03 +0200

    The first commit of angular

---
+++ src/db/data.ts
@@ -0,0 +1,7 @@
+export const cars = [
+    {brand: 'KIA', color: 'black', type: 'sedan', price: 18000, isPresent: true},
+    {brand: 'BMV', color: 'red', type: 'sport coupe', price: 28500, isPresent: false},
+    {brand: 'Ford', color: 'silver', type: 'hatchback', price: 18900, isPresent: false},
+    {brand: 'Nissan', color: 'green', type: 'compact', price: 15000, isPresent: true},
+    {brand: 'Jeep', color: 'black', type: 'crossover', price: 28000, isPresent: true}
+    ];
